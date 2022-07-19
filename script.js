a75a5d0fc9834b9fd973030a545a529de0d366d7
tam-fro <107912266+tam-fro@users.noreply.github.com>
2022-07-19 14:54:37 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+		
+		$('.slider').bxSlider({
+	    mode: 'fade',
+		auto: true,
+		captions: true,
+		speed: 4000
+		});
+		
+   $('a[href^="#"]').click(function() {
+
+				var speed = 500;  
+      var offset = -20;  
+
+      var anchor = $(this).attr("href");
+
+      var target = $(anchor == "#" || anchor == "" ? 'html' : anchor);
+      var position = target.offset().top + offset;
+
+      $('body,html').animate({scrollTop:position}, speed, 'swing');
+
+      return false;
+
+   });
+
+});
+
